8e547dcf3be1b468173ba940024a0dfb51b045ce
shejuan <shejuan@evergrande.com>
2018-10-18 17:52:24 +0800

    解决代码冲突

--- src/index.js
+++ src/index.js
@@ -5,27 +5,18 @@ import MRoute from './routes/router'
 import './index.css'
 import './styles/index.less'
 import registerServiceWorker from './registerServiceWorker'
-<<<<<<< HEAD
 import { HashRouter as Router } from 'react-router-dom'
-
-
-ReactDOM.render(
-  <Router>
-    {/* <App /> */}
-    <MRoute />
-  </Router>,
-=======
-import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
+
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      {/* <App /> */}
+      <MRoute />
     </Router>
   </Provider>,
->>>>>>> 2d539a4f927bf9c09db9470e1c3cd212f5c1689a
   document.getElementById('root')
 )
 registerServiceWorker()
