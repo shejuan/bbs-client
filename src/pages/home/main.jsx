0ed4279ccb2e58e2283b729f6ca849ed4fbdd1a7
zhanxin <936774456@qq.com>
2018-10-19 09:03:15 +0800

    'update'

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -5,7 +5,7 @@ import EditPage from '@/pages/edit/edit'
 import ReplyPage from '@/pages/reply/reply'
 import Home from '@/pages/home/modules/home'
 import { Layout, Menu, Icon } from 'antd'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import './main.less'
 
 const { Content, Sider } = Layout
@@ -56,28 +56,28 @@ class Main extends React.Component {
                 })}
               </Menu>
             </Sider>
-            {/* <Layout style={{ padding: '0 24px 24px', height: '100%' }}> */}
-            <Content
-              style={{
-                background: '#fff',
-                padding: 24,
-                margin: 0,
-                minHeight: 'initial'
-              }}
-            >
-              {/* <Switch> */}
-              <Route path="/" exact component={Home} />
-              <Route path="/edit" component={EditPage} />
-              <Route path="/reply" component={ReplyPage} />
-              <Route path="/questions/ask" component={AskQuestionsPage} />
-              <Route
-                path="/question/detail/:questionId"
-                component={QuestionDetail}
-              />
-              {/* </Switch> */}
-            </Content>
+            <Layout style={{ padding: '0 24px 24px', height: '100%' }}>
+              <Content
+                style={{
+                  background: '#fff',
+                  padding: 24,
+                  margin: 0,
+                  minHeight: 'initial'
+                }}
+              >
+                <Switch>
+                  <Route path="/" exact component={Home} />
+                  <Route path="/edit" component={EditPage} />
+                  <Route path="/reply" component={ReplyPage} />
+                  <Route path="/questions/ask" component={AskQuestionsPage} />
+                  <Route
+                    path="/question/detail/:questionId"
+                    component={QuestionDetail}
+                  />
+                </Switch>
+              </Content>
+            </Layout>
           </Layout>
-          {/* </Layout> */}
         </div>
       </Router>
     )
